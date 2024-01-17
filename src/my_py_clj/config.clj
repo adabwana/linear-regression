c48550e97620bea58f90edfab016f01d416374fd
adabwana <adazungu@gmail.com>
2024-01-16 19:27:32 -0500

    gh-pages clear

--- src/my_py_clj/config.clj
+++
@@ -1,6 +0,0 @@
-(ns my-py-clj.config
-  (:require [libpython-clj2.python :as py]))
-
-;; When you use conda, it should look like this.
-(py/initialize! :python-executable "/opt/anaconda3/envs/my_env/bin/python3.7"
-                :library-path "/opt/anaconda3/envs/my_env/lib/libpython3.7m.dylib")
