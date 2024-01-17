291dac75c38cfaabb71b4cace10f7c80613607b1
adabwana <adazungu@gmail.com>
2024-01-17 16:40:11 -0500

    <br> before &nbsp;

--- src/index.clj
+++ src/index.clj
@@ -8,9 +8,9 @@
 ;* Scale, normalize, and/or encode your data appropriately.
 ;* Implement the Multivariate Linear Regression.
 ;* Compare the performance of your implementation in terms of computation time and other appropriate metrics using 5-fold Cross Validation using a variety of Alpha values.
-;* Modify the "README.md" file to include the following sections:
-;&nbsp;&nbsp;&nbsp;&nbsp;* Summary: A one-paragraph summary of the algorithm that was implemented including any pertinent or useful information. If mathematics are appropriate, include those as well.
-;&nbsp;&nbsp;&nbsp;&nbsp;* Results: Display the comparative analysis of your implementation and the benchmark implementation in terms of computation time and other metrics. Which algorithm performed better? Why do you think this is the case?
+;* Modify the "README.md" file to include the following sections:<br>
+;&nbsp;&nbsp;&nbsp;&nbsp;* Summary: A one-paragraph summary of the algorithm that was implemented including any pertinent or useful information. If mathematics are appropriate, include those as well.<br>
+;&nbsp;&nbsp;&nbsp;&nbsp;* Results: Display the comparative analysis of your implementation and the benchmark implementation in terms of computation time and other metrics. Which algorithm performed better? Why do you think this is the case?<br>
 ;&nbsp;&nbsp;&nbsp;&nbsp;* Reflection: One paragraph describing useful takeaways from the week, things that surprised you, or things that caused you inordinate difficulty.
 ;* Make sure that your README file is formatted properly and is visually appealing. It should be free of grammatical errors, punctuation errors, capitalization issues, etc.
 ;
